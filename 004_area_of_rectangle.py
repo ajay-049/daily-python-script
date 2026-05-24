1dd953d742e1ee1c8a742c3cd65ace2f5a0770eb
Ajay Gupta <ajaygupta805288@gmail.com>
2026-05-24 19:39:15 +0530

    todays programms is commited

---
+++ 004_area_of_rectangle.py
@@ -0,0 +1,4 @@
+X=float(input("Enter the length of the rectangle: "))
+Y=float(input("Enter the width of the rectangle: "))
+Z=X*Y
+print("The area of the rectangle is:", Z)
---
+++ 006_swap_two_variables.py
@@ -0,0 +1,7 @@
+A=float(input("Enter the value of A: "))
+B=float(input("Enter the value of B: "))
+C=A
+A=B
+B=C
+print("The value of A is:", A)
+print("The value of B is:", B)
