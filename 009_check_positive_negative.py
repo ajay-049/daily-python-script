b43625883ea4b9cbb59ef5a250f41a94bf9cbb7a
Ajay Gupta <ajaygupta805288@gmail.com>
2026-05-26 22:54:12 +0530

    todays scripts

---
+++ 009_check_positive_negative.py
@@ -0,0 +1,6 @@
+A=float(input("Enter a number: "))
+if A>0:
+    print("The number is positive.")
+elif A<0:
+    print("The number is negative.")
+else:    print("The number is zero.")
---
+++ 010_even_or_odd.py
@@ -0,0 +1,5 @@
+A=float(input("Enter a number: "))
+if A%2==0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
