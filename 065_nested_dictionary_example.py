ff5560106facd1af970ecf437f3936f8be28d5ff
Ajay Gupta <ajaygupta805288@gmail.com>
2026-07-06 22:52:53 +0530

    todays second python script

---
+++ 065_nested_dictionary_example.py
@@ -0,0 +1,5 @@
+employees = {
+    "emp1": {"name": "Alice", "role": "Developer", "salary": 85000},
+    "emp2": {"name": "Bob", "role": "Designer", "salary": 70000}
+}
+print(employees)
---
+++ 066_simple_function.py
@@ -0,0 +1,6 @@
+a=int(input("Enter a number: "))
+b=int(input("Enter another number: "))
+
+def add(a,b):
+    return a+b
+print(add(a, b))
